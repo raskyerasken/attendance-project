9fccd5f8553df18f4993a16e314c906a0623fa39
skobis <32453415+skobis@users.noreply.github.com>
2018-02-22 11:35:01 +0100

    khgi

---
+++ Attendance/src/attendance/VaizdasController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Skomantas
+ */
+public class VaizdasController implements Initializable {
+   
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+}
