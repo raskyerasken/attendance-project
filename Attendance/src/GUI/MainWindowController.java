8b43ae11fded8a363d341800123dfaaac5da7ed7
Sub-Z3r0 <andersen19@gmail.com>
2018-04-06 12:34:53 +0200

    datepicker worx

--- Attendance/src/GUI/MainWindowController.java
+++ Attendance/src/GUI/MainWindowController.java
@@ -78,7 +78,7 @@ public class MainWindowController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb) 
     { DataBasePresentDate ads= new DataBasePresentDate();
-    ads.removeall();
+    
             
 //        MockData md = new MockData();
 //        try 
